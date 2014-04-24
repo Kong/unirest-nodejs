c5d6d4392494d8989da1615571b857ee9668f68c
Nijiko Yonskai <nijikokun@gmail.com>
2014-04-24 12:03:43 -0700

    Fix trim on booleans

--- index.js
+++ index.js
@@ -371,9 +371,13 @@ Unirest = function (method, uri, headers, body, callback) {
           } else {
             function setCookie (cookie) {
               var crumbs = Unirest.trim(cookie).split('=');
+              var key = Unirest.trim(crumbs[0]);
+              var value = Unirest.trim(crumbs.slice(1).join('='));
 
               if (crumbs[0] && crumbs[0] != '') {
-                result.cookies[Unirest.trim(crumbs[0])] = Unirest.trim(crumbs.slice(1).join('=') || true);
+                result.cookies[key] = value === ''
+                  ? true
+                  : value;
               }
             }
 
